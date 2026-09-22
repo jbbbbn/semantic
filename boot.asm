1d810729737030c6c3c19cf0d57b9091088ae234
jbbbbn <giammabot73@gmail.com>
2026-09-22 21:36:23 +0200

    feat: added 'TIME' command + boot and kernel files

--- boot.asm
+++ boot.asm
@@ -1,211 +1,35 @@
 [org 0x7C00]
 bits 16
 
+KERNEL_OFFSET equ 0x8000
+
 start:
-    MOV SI, messaggio
-    CALL print
+    MOV [BOOT_DRIVE], DL
 
-    CALL newline
+    XOR AX, AX
+    MOV DS, AX
+    MOV ES, AX
+    MOV SS, AX
+    MOV SP, 0x7C00
 
-    JMP main
+    MOV BX, KERNEL_OFFSET
+    MOV DH, 4
+    MOV DL, [BOOT_DRIVE]
+    CALL disk_load
 
-; ==========================================
-; MAIN TERMINAL
-; ==========================================
+    JMP KERNEL_OFFSET
 
-main:
-    MOV DI, input_buffer
-    MOV BYTE [DI], 0
-
-    MOV SI, prompt
-    CALL print
-
-    .input:
-        ; Wait for a key
-        MOV AH, 0x00
-        INT 0x16
-
-        ; Check for ENTER
-        CMP AL, 0x0D
-        JE .execute
-
-        ; Store character in buffer
-        MOV [DI], AL
-        INC DI
-
-        ; Echo character
-        MOV AH, 0x0E
-        INT 0x10
-
-        JMP .input
-
-    .execute:
-        ; Null-terminate input
-        MOV BYTE [DI], 0
-
-        CALL newline
-
-        MOV SI, input_buffer
-        MOV DI, cmd_ver
-        CALL strcmp
-        JE .do_ver
-
-        MOV SI, input_buffer
-        MOV DI, cmd_help
-        CALL strcmp
-        JE .do_help
-
-        MOV SI, input_buffer
-        MOV DI, cmd_cls
-        CALL strcmp
-        JE .do_cls
-
-        JMP .unknown_command
-
-        .do_ver:
-            CALL ver
-            JMP main
-
-        .do_help:
-            CALL help
-            JMP main
-
-        .do_cls:
-            CALL cls
-            JMP main
-
-        .unknown_command:
-            CALL unknown
-            JMP main
-
-; ==========================================
-; FUNCTIONS
-; ==========================================
-
-newline:
-    MOV AH, 0x0E
-
-    MOV AL, 0x0D
-    INT 0x10
-
-    MOV AL, 0x0A
-    INT 0x10
-
-    RET
-
-print:
-    MOV AH, 0x0E
-
-    .loop:
-        LODSB
-        CMP AL, 0
-        JE .done
-
-        INT 0x10
-        JMP .loop
-
-    .done:
-        RET
-
-strcmp:
-    .loop:
-        MOV AL, [SI]      ; character string 1
-        MOV BL, [DI]      ; character string 1
-
-        CMP AL, BL
-        JNE .not_equal    ; if different, exit
-
-        CMP AL, 0
-        JE .equal         ; if both 0, they're both equal
-
-        INC SI            ; next character
-        INC DI
-        JMP .loop
-
-    .not_equal:
-        RET               ; ZF = 0
-
-    .equal:
-        RET               ; ZF = 1
-
-ver:
-    MOV SI, ver_text
-    CALL print
-    CALL newline
-    RET
-
-help:
-    MOV SI, help_text
-    CALL print
-    CALL newline
-    RET
-
-cls:
-    MOV AH, 0x06      ; Scroll Up Window
-    MOV AL, 0x00      ; 0 = clear entire window
-
-    MOV BH, 0x07      ; White on black
-
-    MOV CX, 0x0000    ; Top-left corner
-    MOV DX, 0x184F    ; Bottom-right (25x80)
-
-    INT 0x10
-
-    ; Move cursor to (0,0)
+disk_load:
     MOV AH, 0x02
-    MOV BH, 0x00
+    MOV AL, DH
+    MOV CH, 0x00
+    MOV CL, 0x02
     MOV DH, 0x00
-    MOV DL, 0x00
-    INT 0x10
-
+    INT 0x13
     RET
 
-unknown:
-    MOV SI, unknown_text
-    CALL print
-    CALL newline
-    RET
-
-; ==========================================
-; DATA
-; ==========================================
-
-messaggio:
-    db "Semantic", 0
-
-prompt:
-    db "S:> ", 0
-
-input_buffer:
-    times 64 db 0
-; ==========================================
-; SYSTEM COMMANDS
-
-cmd_ver:
-    db "VER", 0
-
-; ==========================================
-
-cmd_help:
-    db "HELP", 0
-
-cmd_cls:
-    db "CLS", 0
-
-help_text:
-    db "CLS   - clears the screen",0x0D,0x0A
-    db "HELP  - shows available commands",0x0D,0x0A
-    db "VER   - shows system version",0
-
-unknown_text:
-    db "Unknown command", 0
-
-ver_text:
-    db "Semantic 0.6", 0
-
-; ==========================================
-; BOOT SIGNATURE
-; ==========================================
+BOOT_DRIVE:
+    db 0
 
 times 510-($-$$) db 0
 dw 0xAA55
---
+++ kernel.asm
@@ -0,0 +1,256 @@
+[org 0x8000]
+bits 16
+
+start:
+    MOV SI, messaggio
+    CALL print
+    CALL newline
+    JMP main
+
+; ==========================================
+; MAIN TERMINAL
+; ==========================================
+
+main:
+    MOV DI, input_buffer
+    MOV BYTE [DI], 0
+
+    MOV SI, prompt
+    CALL print
+
+.input:
+    ; Wait for a key
+    MOV AH, 0x00
+    INT 0x16
+
+    ; Check for ENTER
+    CMP AL, 0x0D
+    JE .execute
+
+    ; Store character in buffer
+    MOV [DI], AL
+    INC DI
+
+    ; Echo character
+    MOV AH, 0x0E
+    INT 0x10
+
+    JMP .input
+
+.execute:
+    ; Null-terminate input
+    MOV BYTE [DI], 0
+    CALL newline
+
+    ; CLS
+    MOV SI, input_buffer
+    MOV DI, cmd_cls
+    CALL strcmp
+    JE .do_cls
+
+    ; HELP
+    MOV SI, input_buffer
+    MOV DI, cmd_help
+    CALL strcmp
+    JE .do_help
+
+    ; TIME
+    MOV SI, input_buffer
+    MOV DI, cmd_time
+    CALL strcmp
+    JE .do_time
+
+    ; VER
+    MOV SI, input_buffer
+    MOV DI, cmd_ver
+    CALL strcmp
+    JE .do_ver
+
+    JMP .unknown_command
+
+.do_cls:
+    CALL cls
+    JMP main
+
+.do_help:
+    CALL help
+    JMP main
+
+.do_time:
+    CALL time
+    JMP main
+
+.do_ver:
+    CALL ver
+    JMP main
+
+.unknown_command:
+    CALL unknown
+    JMP main
+
+; ==========================================
+; FUNCTIONS
+; ==========================================
+
+newline:
+    MOV AH, 0x0E
+
+    MOV AL, 0x0D
+    INT 0x10
+
+    MOV AL, 0x0A
+    INT 0x10
+
+    RET
+
+print:
+    MOV AH, 0x0E
+
+.loop:
+    LODSB
+    CMP AL, 0
+    JE .done
+
+    INT 0x10
+    JMP .loop
+
+.done:
+    RET
+
+print_bcd:
+    PUSH BX
+
+    MOV BL, AL
+
+    ; High digit
+    SHR AL, 4
+    ADD AL, '0'
+    MOV AH, 0x0E
+    INT 0x10
+
+    ; Low digit
+    MOV AL, BL
+    AND AL, 0x0F
+    ADD AL, '0'
+    MOV AH, 0x0E
+    INT 0x10
+
+    POP BX
+    RET
+
+strcmp:
+.loop:
+    MOV AL, [SI]
+    MOV BL, [DI]
+
+    CMP AL, BL
+    JNE .not_equal
+
+    CMP AL, 0
+    JE .equal
+
+    INC SI
+    INC DI
+    JMP .loop
+
+.not_equal:
+    RET
+
+.equal:
+    RET
+
+cls:
+    MOV AH, 0x06
+    MOV AL, 0x00
+    MOV BH, 0x07
+    MOV CX, 0x0000
+    MOV DX, 0x184F
+    INT 0x10
+
+    ; Move cursor to (0,0)
+    MOV AH, 0x02
+    MOV BH, 0x00
+    MOV DH, 0x00
+    MOV DL, 0x00
+    INT 0x10
+
+    RET
+
+help:
+    MOV SI, help_text
+    CALL print
+    CALL newline
+    RET
+
+time:
+    MOV AH, 0x02
+    INT 0x1A
+
+    MOV AL, CH
+    CALL print_bcd
+
+    MOV AL, ':'
+    MOV AH, 0x0E
+    INT 0x10
+
+    MOV AL, CL
+    CALL print_bcd
+
+    MOV AL, ':'
+    MOV AH, 0x0E
+    INT 0x10
+
+    MOV AL, DH
+    CALL print_bcd
+
+    CALL newline
+    RET
+
+unknown:
+    MOV SI, unknown_text
+    CALL print
+    CALL newline
+    RET
+
+ver:
+    MOV SI, ver_text
+    CALL print
+    CALL newline
+    RET
+
+; ==========================================
+; DATA
+; ==========================================
+
+messaggio:
+    db "Semantic", 0
+
+prompt:
+    db "S:> ", 0
+
+input_buffer:
+    times 64 db 0
+
+cmd_cls:
+    db "CLS", 0
+
+cmd_help:
+    db "HELP", 0
+
+cmd_time:
+    db "TIME", 0
+
+cmd_ver:
+    db "VER", 0
+
+help_text:
+    db "CLS   - clears the screen",0x0D,0x0A
+    db "HELP  - shows available commands",0x0D,0x0A
+    db "TIME  - shows current system time",0x0D,0x0A
+    db "VER   - shows system version",0
+
+unknown_text:
+    db "Unknown command", 0
+
+ver_text:
+    db "Semantic 0.8", 0
